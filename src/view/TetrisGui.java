0a87f741272f21c75569650520411f67f176582b
ddxbugs <ddxbugs@proton.me>
2023-05-13 16:45:10 -0700

    view/TetrisGui.java set tetris view layout panels

--- src/view/TetrisGui.java
+++ src/view/TetrisGui.java
@@ -8,28 +8,25 @@ import java.awt.HeadlessException;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 /**
  * @author ddxbugs
  *
  */
 public class TetrisGui extends JFrame implements Observer {
+	
 	private BoardView myBoardView;
 	private PreviewPanel myPreviewPanel;
 	private ScorePanel myScorePanel;
 	
 	/**
-	 * Invokes the runnable method
-	 */
-	public void start() {
-		
-	}
-	
-	/**
 	 * @throws HeadlessException
 	 */
-	public TetrisGui() throws HeadlessException {
+	private TetrisGui() throws HeadlessException {
 		// TODO Auto-generated constructor stub
 	}
 
@@ -48,11 +45,23 @@ public class TetrisGui extends JFrame implements Observer {
 	public TetrisGui(String title) throws HeadlessException {
 		super(title);
 		// TODO Auto-generated constructor stub
+		JLayeredPane layeredPane;
+		JPanel panel;
+		layeredPane = new JLayeredPane();
+		panel = new JPanel();
+		
+		layeredPane.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
 		myBoardView = new BoardView();
 		myPreviewPanel = new PreviewPanel();
 		myScorePanel = new ScorePanel();
+		
+		panel.add(myScorePanel);
+		panel.add(myBoardView);
+		panel.add(myPreviewPanel);
+		
+		layeredPane.add(panel);
 	}
-
 	/**
 	 * @param title
 	 * @param gc

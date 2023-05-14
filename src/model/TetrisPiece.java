e03722d8c4db102d731d763cdad34b2808819663
ddxbugs <ddxbugs@proton.me>
2023-05-14 12:48:13 -0700

    model/TetrisPiece.java initial commit

---
+++ src/model/TetrisPiece.java
@@ -0,0 +1,19 @@
+/**
+ * TetrisPiece.java
+ */
+package model;
+
+/**
+ * @author ddxbugs
+ *
+ */
+class TetrisPiece {
+
+	/**
+	 * 
+	 */
+	public TetrisPiece() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

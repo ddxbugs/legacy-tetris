d7007471bb61b859f109f2688265aebba45eeab8
ddxbugs <ddxbugs@proton.me>
2023-05-14 12:56:56 -0700

    model/BoardModel.java initialize tetris new game pieces

--- src/model/BoardModel.java
+++ src/model/BoardModel.java
@@ -3,17 +3,37 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
 /**
  * @author ddxbugs
  *
  */
-public class BoardModel {
-
+public class BoardModel extends Observable {
+	private static final int DEFAULT_WIDTH = 10;
+	private static final int DEFAULT_HEIGHT = 20;
+	
+	private List<Block[]> myFrozenBlocks;
+	private List<TetrisPiece> myTetrisPieces;
+	private TetrisPiece myNextPiece;
+	private MovableTetrisPiece myCurrentPiece;
+	
+	
 	/**
 	 * 
 	 */
 	public BoardModel() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub'
+		myFrozenBlocks = new ArrayList<Block[]>();
+		myTetrisPieces = new ArrayList<TetrisPiece>();
+		myNextPiece = null;
+		myCurrentPiece = null;
+		
+	}
+	public void down() {
+	
 	}
 
 }

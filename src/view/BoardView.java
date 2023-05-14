6017ccaddf1e49b8601efb3fc234d1e530068bbb
ddxbugs <ddxbugs@proton.me>
2023-05-13 17:01:17 -0700

    view/BoardView.java add tetris board view model dependency

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -4,20 +4,28 @@
 package view;
 
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import model.BoardModel;
 /**
  * 
  * @author ddxbugs
  *
  */
-public class BoardView extends JPanel {
-
+public class BoardView extends JPanel implements ActionListener {
+	private static final int DEFAULT_DELAY_MS = 1000;
+	private BoardModel myBoardModel;
 	private Timer myTimer;
 	
 	public BoardView() {
 		// TODO Auto-generated constructor stub
+		myBoardModel = new BoardModel();
+		myTimer = new Timer(DEFAULT_DELAY_MS, this);
+		
 	}
 
 	public BoardView(LayoutManager layout) {
@@ -37,6 +45,12 @@ public class BoardView extends JPanel {
 	
 	protected void endGame() {
 		myTimer.stop();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+//		myBoardModel.down();
 	}
 	
 }

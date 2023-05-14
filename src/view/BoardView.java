4b6717509a5a37b792bfbab0215e83d77d09ce72
ddxbugs <ddxbugs@proton.me>
2023-05-14 12:59:17 -0700

    view/BoardView.java setup tetris game swing timer mechanics

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -18,11 +18,15 @@ import model.BoardModel;
  */
 public class BoardView extends JPanel implements ActionListener {
 	private static final int DEFAULT_DELAY_MS = 1000;
+	private static final int ONE_HUNDRETH_MS = 100;
 	private BoardModel myBoardModel;
 	private Timer myTimer;
+	private int myLevel;
+	
 	
 	public BoardView() {
 		// TODO Auto-generated constructor stub
+		super();
 		myBoardModel = new BoardModel();
 		myTimer = new Timer(DEFAULT_DELAY_MS, this);
 		
@@ -43,14 +47,24 @@ public class BoardView extends JPanel implements ActionListener {
 		// TODO Auto-generated constructor stub
 	}
 	
+	protected void newGame() {
+		myTimer.start();
+	}
 	protected void endGame() {
+		myLevel = 0;
 		myTimer.stop();
 	}
-
+	protected void setDelay(final int theLevel) {
+		myTimer.setDelay(theLevel * ONE_HUNDRETH_MS);
+	}
+	protected void setLevel(final int theLevel) {
+		myLevel = theLevel;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-//		myBoardModel.down();
+		myBoardModel.down();
 	}
 	
 }

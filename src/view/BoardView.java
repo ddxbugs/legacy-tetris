89fe1a7482fed092ed0794b1aff084952620e7dd
ddxbugs <ddxbugs@proton.me>
2023-05-15 15:24:30 -0700

    view/BoardView.java rm redundant constructor method and add graphic rendering

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -3,7 +3,10 @@
  */
 package view;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.LayoutManager;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,53 +21,50 @@ import model.BoardModel;
  */
 public class BoardView extends JPanel implements ActionListener {
 	private static final int DEFAULT_DELAY_MS = 1000;
+	private static final int DEFAULT_BOARD_MODEL_SCALE = 30;
 	private static final int ONE_HUNDRETH_MS = 100;
 	private BoardModel myBoardModel;
 	private Timer myTimer;
-	private int myLevel;
-	
 	
 	public BoardView() {
 		// TODO Auto-generated constructor stub
 		super();
 		myBoardModel = new BoardModel();
 		myTimer = new Timer(DEFAULT_DELAY_MS, this);
-		
-	}
-
-	public BoardView(LayoutManager layout) {
-		super(layout);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BoardView(boolean isDoubleBuffered) {
-		super(isDoubleBuffered);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BoardView(LayoutManager layout, boolean isDoubleBuffered) {
-		super(layout, isDoubleBuffered);
-		// TODO Auto-generated constructor stub
 	}
 	
+	public void setDimension(final int theWidth, final int theHeight, final int theScale) {
+		setSize(theWidth * theScale, theHeight * theScale);
+	}
 	protected void newGame() {
 		myTimer.start();
+//		myBoardModel.reset();
 	}
 	protected void endGame() {
-		myLevel = 0;
 		myTimer.stop();
 	}
 	protected void setDelay(final int theLevel) {
 		myTimer.setDelay(theLevel * ONE_HUNDRETH_MS);
 	}
-	protected void setLevel(final int theLevel) {
-		myLevel = theLevel;
-	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		myBoardModel.down();
+	}
+	
+	public void paintComponent(final Graphics theGraphics) {
+		super.paintComponent(theGraphics);
+		final Graphics2D g = (Graphics2D) theGraphics;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		drawBlocks(g);
+	}
+	
+	private void drawBlocks(final Graphics2D theGraphics) {
+		int rectX, rectY;
+		
 	}
 	
 }

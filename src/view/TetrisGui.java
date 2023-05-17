ddf961af9933afb7012f5276cf500bac5bb90403
ddxbugs <ddxbugs@proton.me>
2023-05-16 18:49:27 -0700

    view/TetrisGui.java add ui basic menu selection items

--- src/view/TetrisGui.java
+++ src/view/TetrisGui.java
@@ -5,12 +5,18 @@ package view;
 
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -19,6 +25,9 @@ import javax.swing.JPanel;
  */
 public class TetrisGui extends JFrame implements Observer {
 
+	private static final String INFO = "Disney's : Tron 2010 All Rights Reserved";
+	private static final String TITLE = "Legacy Tetris 2018";
+	
 	private static final int DEFAULT_BOARD_WIDTH = 300;
 	private static final int DEFAULT_BOARD_HEIGHT = 600;
 	private static final int DEFAULT_PREVIEW_PANEL_WIDTH = 0;
@@ -58,13 +67,14 @@ public class TetrisGui extends JFrame implements Observer {
 		
 		add(layeredPane);
 	}
-	
+	/**
+	 * 
+	 */
 	private void setUpComponents() {
 		myBoardView.setSize(DEFAULT_BOARD_WIDTH, DEFAULT_BOARD_HEIGHT);
 		myPreviewPanel.setSize(DEFAULT_PREVIEW_PANEL_WIDTH, DEFAULT_PREVIEW_PANEL_HEIGHT);
 		myScorePanel.setSize(DEFAULT_SCORE_PANEL_WIDTH, DEFAULT_SCORE_PANEL_HEIGHT);
 	}
-	
 	/**
 	 * 
 	 */
@@ -77,5 +87,73 @@ public class TetrisGui extends JFrame implements Observer {
 			myBoardView.endGame();
 		}
 	}
-
+	
+	/**
+	 * @author ddxbugs
+	 *
+	 */
+	class MenuBar extends JMenuBar implements ActionListener {
+		
+		private static final String ABOUT = "About";
+		private static final String NEW_GAME = "New Game";
+		private static final String END_GAME = "End Game";
+		private static final String SETTINGS = "Settings";
+		private static final String HELP = "Help";
+		private static final String EXIT = "Exit";
+		private static final String CONTROLS = "Controls";
+		MenuBar() {
+			super();
+			setUpComponents();
+			setVisible(true);
+			
+		}
+		/**
+		 * 
+		 */
+		private void setUpComponents() {
+			JMenu newGame = new JMenu(NEW_GAME);
+			JMenu settings = new JMenu(SETTINGS);
+			JMenu endGame = new JMenu(END_GAME);
+			
+			JMenuItem exit = new JMenuItem(EXIT);
+			JMenuItem about = new JMenuItem(ABOUT);
+			JMenuItem help = new JMenuItem(HELP);
+			JMenuItem controls = new JMenuItem(CONTROLS);
+			
+			newGame.addActionListener(this);
+			endGame.addActionListener(this);
+			settings.addActionListener(this);
+			exit.addActionListener(this);
+			about.addActionListener(this);
+			help.addActionListener(this);
+			controls.addActionListener(this);
+			
+			settings.add(about);
+			settings.add(help);
+			settings.add(controls);
+			
+			add(newGame);
+			add(endGame);
+			add(settings);
+		}
+		/**
+		 * 
+		 */
+		@Override
+		public void actionPerformed(ActionEvent theActionEvent) {
+			// TODO Auto-generated method stub
+			final String cmd = theActionEvent.getActionCommand();
+			
+			switch(cmd) {
+			case NEW_GAME : break;
+			case END_GAME : break;
+			case SETTINGS : break;
+			case ABOUT : JOptionPane.showMessageDialog(myBoardView, INFO, TITLE, JOptionPane.OK_OPTION);
+			case HELP : break;
+			case EXIT : break;
+			case CONTROLS : break;
+			default : break;
+			}
+		}
+	}
 }

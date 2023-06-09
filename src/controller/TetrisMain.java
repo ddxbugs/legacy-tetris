30f6551e62342256ff84c795d4379aab0b7fd369
ddxbugs <ddxbugs@proton.me>
2023-06-08 20:50:38 -0700

    controller/TetrisMain update graphics environment for full screen mode

--- src/controller/TetrisMain.java
+++ src/controller/TetrisMain.java
@@ -5,6 +5,8 @@ package controller;
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.net.URL;
 import java.util.Arrays;
@@ -21,8 +23,12 @@ import view.TetrisGui;
 public class TetrisMain {
 	private static final String TITLE = "Disney's Tron: Legacy Tetris";
 	private static final String ICON = "/res/icon.jpg";
+	/** The default gui frame width */
 	private static final int DEFAULT_GUI_WIDTH = 1000;
-	private static final int DEFAULT_GUI_HEIGHT = 700;
+	/** The default gui frame height */
+	private static final int DEFAULT_GUI_HEIGHT = 600;
+	
+	private static GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	/**
 	 * Private constructor inhibits instantiation
 	 */
@@ -35,22 +41,29 @@ public class TetrisMain {
 	 * @param args the Arguments
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Arrays.toString(args));
+		
+		GraphicsDevice graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				
 				final URL url = getClass().getResource(ICON);
 				final Image icon = new ImageIcon(url).getImage();
 				
 				final TetrisGui gui = new TetrisGui(TITLE);
 				gui.setIconImage(icon);
 				gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				gui.setMinimumSize(new Dimension(DEFAULT_GUI_WIDTH, DEFAULT_GUI_HEIGHT));
 				gui.setResizable(false);
 				gui.pack();
 				gui.setLocationRelativeTo(null);
 				gui.setVisible(true);
+				
+				if (graphicsDevice.isFullScreenSupported()) {
+					graphicsDevice.setFullScreenWindow(gui);
+					gui.validate();
+				} else {
+					gui.setSize(new Dimension(DEFAULT_GUI_WIDTH, DEFAULT_GUI_HEIGHT));
+				}
 			}
 		});
 

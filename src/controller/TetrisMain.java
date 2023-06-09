54ce87361464f82b397cc7662d320fe150d34783
ddxbugs <ddxbugs@proton.me>
2023-06-08 19:57:35 -0700

    controller/TetrisMain add gui image icon

--- src/controller/TetrisMain.java
+++ src/controller/TetrisMain.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Image;
 import java.net.URL;
+import java.util.Arrays;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -20,12 +21,13 @@ import view.TetrisGui;
 public class TetrisMain {
 	private static final String TITLE = "Disney's Tron: Legacy Tetris";
 	private static final String ICON = "/res/icon.jpg";
-	private static final int DEFAULT_WIDTH = 600;
-	private static final int DEFAULT_HEIGHT = 700;
+	private static final int DEFAULT_GUI_WIDTH = 1000;
+	private static final int DEFAULT_GUI_HEIGHT = 700;
 	/**
 	 * Private constructor inhibits instantiation
 	 */
 	private TetrisMain() {
+		System.err.println("error:"+IllegalStateException.class);
 		throw new IllegalStateException();
 	}
 	/**
@@ -34,6 +36,7 @@ public class TetrisMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(Arrays.toString(args));
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -41,8 +44,9 @@ public class TetrisMain {
 				final Image icon = new ImageIcon(url).getImage();
 				
 				final TetrisGui gui = new TetrisGui(TITLE);
+				gui.setIconImage(icon);
 				gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				gui.setMinimumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+				gui.setMinimumSize(new Dimension(DEFAULT_GUI_WIDTH, DEFAULT_GUI_HEIGHT));
 				gui.setResizable(false);
 				gui.pack();
 				gui.setLocationRelativeTo(null);
